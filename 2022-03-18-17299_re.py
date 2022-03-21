8e82d2a2cbc6afd3bc199a4da7b2f37d16e01b05
손정균_T3111 <wjdrbs51@koreatech.ac.kr>
2022-03-21 09:25:29 +0900

    coding

---
+++ 2022-03-18-17299_re.py
@@ -0,0 +1,14 @@
+import sys
+from collections import Counter
+
+num = int(input())
+n = list(map(int,sys.stdin.readline().split()))
+counter = Counter(n)
+re = [-1]*num
+stack = [0]
+
+for i in range(1,num):
+    while counter[n[i]] > counter[n[stack[-1]]]:
+        re[stack.pop()] = n[i]
+    stack.append(i)
+print(re)
